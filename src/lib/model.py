a2f55c0bccd9d08d4b39d66d9de2b5e7996cabd9
Stefan Nožinić <stefan@lugons.org>
2016-11-06 17:21:03 +0100

    Setup initial infrastructure for model

---
+++ src/lib/model.py
@@ -0,0 +1,30 @@
+
+
+class NoSignalException(Exception):
+    pass
+
+class NotTrainedException(Exception):
+    pass
+
+class PCAModel(object):
+
+    def __init__(self):
+        pass
+
+    def train(self, data):
+        pass
+
+    def classify(self, sample):
+        pass
+
+    def validate(self):
+        pass
+
+    def get_eigenvectors(self):
+        pass
+
+    def get_mean(self):
+        pass
+
+    def connect(self, event, fun):
+        pass
